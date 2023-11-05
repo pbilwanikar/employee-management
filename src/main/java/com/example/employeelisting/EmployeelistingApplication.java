582189c4895876d583ebcae42d68ae1c7c9d8c32
pbilwanikar <55834905+pbilwanikar@users.noreply.github.com>
2023-11-05 22:25:03 +0530

    base project for employee database management project

---
+++ src/main/java/com/example/employeelisting/EmployeelistingApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeelisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeelistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeelistingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeelisting/EmployeelistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeelisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeelistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
